39ca3f0e23d6bc39aac14ccdd4148eb752fdff15
Sonu <sonu3660@gmail.com>
2020-08-09 16:12:45 +0530

    Deploy Cart app on firebase

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,64 +1,60 @@
-import React from 'react';
+import React from "react";
 
 // Change into function
-const CartItem = (props) =>{
-    
-        const { price, title, qty }= props.product;
-        const {
-            product,
-            onIncreaseQuantity,
-            onDecreaseQuantity,
-            onDeleteProduct
-        } = props;
+const CartItem = (props) => {
+   // DeStructuring
+   const { price, title, qty } = props.product;
 
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img  style={ styles.images } src={product.img} alt="" />
-                </div>
-                <div className="right-block">
-                    <div style={{ fontSize: 25 }} > {title} </div>
-                    <div style={{ color: '#777' }} >Rs. {price} </div>
-                    <div style={{ color: '#777' }} >Qty: {qty} </div>
-                    <div className="cart-item-actions">
-                        
-                        <img 
-                            alt="increase" 
-                            className="action-icons" 
-                            src="https://image.flaticon.com/icons/svg/1828/1828926.svg" 
+   // DeStructuring
+   const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      onDeleteProduct,
+   } = props;
 
-                            
-                            onClick = { () => onIncreaseQuantity(product)}
-
-                        />
-                        <img 
-                            alt="decrease" 
-                            className="action-icons" 
-                            src="https://image.flaticon.com/icons/svg/1828/1828906.svg" 
-                            
-                            // Decrease qty by props
-                            onClick = { () => onDecreaseQuantity(product)}
-                            />
-                        <img 
-                            alt="delete" 
-                            className="action-icons" 
-                            src="https://image.flaticon.com/icons/svg/1214/1214428.svg" 
-                            onClick={() => onDeleteProduct(product.id)}
-                        />
-
-                    </div>
-                </div>
+   return (
+      <div className="cart-item">
+         <div className="left-block">
+            <img style={styles.images} src={product.img} alt="" />
+         </div>
+         <div className="right-block">
+            <div style={{ fontSize: 25 }}> {title} </div>
+            <div style={{ color: "#777" }}>Rs. {price} </div>
+            <div style={{ color: "#777" }}>Qty: {qty} </div>
+            <div className="cart-item-actions">
+               <img
+                  alt="increase"
+                  className="action-icons"
+                  src="https://image.flaticon.com/icons/svg/1828/1828926.svg"
+                  onClick={() => onIncreaseQuantity(product)}
+               />
+               <img
+                  alt="decrease"
+                  className="action-icons"
+                  src="https://image.flaticon.com/icons/svg/1828/1828906.svg"
+                  // Decrease qty by props
+                  onClick={() => onDecreaseQuantity(product)}
+               />
+               <img
+                  alt="delete"
+                  className="action-icons"
+                  src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+                  onClick={() => onDeleteProduct(product.id)}
+               />
             </div>
-        );
-}
+         </div>
+      </div>
+   );
+};
 
-const styles={
-    images: {
-        height: 130,
-        width: 130,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+const styles = {
+   images: {
+      height: 130,
+      width: 130,
+      borderRadius: 4,
+      background: "#ccc",
+   },
+};
 
 export default CartItem;
